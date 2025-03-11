70ff1d014fccae4977d156e544e21e6730ebca66
ostafiichuko <issimo333@gmail.com>
2025-03-11 10:37:09 +0200

    feat: create db, model and migration

---
+++ conf/db.py
@@ -0,0 +1,18 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+user = os.getenv("USER")
+password = os.getenv("PASSWORD")
+host = os.getenv("HOST")
+port = os.getenv("PORT")
+db_name = os.getenv("DB_NAME")
+
+URI = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
+engine = create_engine(URI)
+SessionLocal = sessionmaker(bind=engine)
