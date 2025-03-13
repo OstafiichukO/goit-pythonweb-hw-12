731dbff8179441779f81f77dd6b43f097819cf87
ostafiichuko <issimo333@gmail.com>
2025-03-13 11:07:57 +0200

    feat: hw 08

--- conf/db.py
+++ conf/db.py
@@ -12,7 +12,7 @@ host = os.getenv("HOST")
 port = os.getenv("PORT")
 db_name = os.getenv("DB_NAME")
 
-URI = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+URI = f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{db_name}"
 
 engine = create_engine(URI)
 SessionLocal = sessionmaker(bind=engine)
