ac93aeb47c0ea63b5113550b290efa5370bfd657
ostafiichuko <issimo333@gmail.com>
2025-04-01 15:20:43 +0300

    feat: added auth and conyacts integration tests and dockstrings

--- src/conf/messages.py
+++ src/conf/messages.py
@@ -3,5 +3,9 @@ Messages used in the application
  """
 
 CONTACT_NOT_FOUND = "Contact not found"
-WELCOME_MESSAGE = "Welcome to FastAPI!"
+WELCOME_MESSAGE = "Welcome to FastAPI!\nTo see documentation goto /docs"
 HEALTHCHECKER_MESSAGE = "App is healthy"
+
+API_ERROR_USER_ALREADY_EXIST = "A user with this email already exists"
+API_ERROR_USER_NOT_AUTHORIZED = "Email address not verified"
+API_ERROR_WRONG_PASSWORD = "Wrong login or password"
